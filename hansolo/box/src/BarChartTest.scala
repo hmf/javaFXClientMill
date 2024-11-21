a149928872d84e6e04b0c1faa7ec87325203b562
hmf <hmf@inescporto.pt>
2024-11-21 17:27:13 +0000

    hansolo.box - v2

--- hansolo/box/src/BarChartTest.scala
+++ hansolo/box/src/BarChartTest.scala
@@ -3,32 +3,21 @@
 package hansolo.charts
 
 
-import eu.hansolo.fx.charts.BarChart
-import eu.hansolo.fx.charts.BarChartBuilder
+import eu.hansolo.fx.charts.BoxPlot
+import eu.hansolo.fx.charts.BoxPlotBuilder
 import eu.hansolo.fx.charts.data.ChartItem
-import eu.hansolo.fx.charts.data.ChartItemBuilder
-import eu.hansolo.fx.charts.series.ChartItemSeries
-import eu.hansolo.fx.charts.series.ChartItemSeriesBuilder
-import eu.hansolo.fx.charts.tools.NumberFormat
-import eu.hansolo.fx.charts.tools.Order
-import javafx.animation.AnimationTimer
 import javafx.application.Application
 import javafx.geometry.Insets
-import javafx.geometry.Orientation
 import javafx.scene.Scene
 import javafx.scene.layout.AnchorPane
 import javafx.scene.layout.Background
 import javafx.scene.layout.BackgroundFill
 import javafx.scene.layout.CornerRadii
 import javafx.scene.paint.Color
-import javafx.scene.paint.CycleMethod
-import javafx.scene.paint.LinearGradient
-import javafx.scene.paint.Stop
 import javafx.stage.Stage
 
 import java.util.ArrayList
 import java.util.List
-import java.util.Random
 
 import scala.compiletime.uninitialized
 
@@ -56,85 +45,60 @@ import scala.compiletime.uninitialized
  * @see https://github.com/HanSolo/charts/blob/master/src/test/java/eu/hansolo/fx/charts/LineChartTest.java
  */
 class BoxPlotTest extends Application {
-    private val RND:           Random = new Random()
-    private var chart:         BarChart[? <: ChartItem] = uninitialized
-    private var items:         List[ChartItem]          = uninitialized
-    private var lastTimerCall: Long                     = uninitialized
-    private var timer:         AnimationTimer           = uninitialized
+    private var boxPlot: BoxPlot[ChartItem] = uninitialized
+    private var items:   List[ChartItem]    = uninitialized
 
 
     override def init() = {
         items = new ArrayList[ChartItem]()
-        items.add(ChartItemBuilder.create().name("Item 1").value(0).fill(Color.rgb(221, 78, 77)).build())
-        items.add(ChartItemBuilder.create().name("Item 2").value(0).fill(Color.rgb(215, 131, 79)).build())
-        items.add(ChartItemBuilder.create().name("Item 3").value(0).fill(Color.rgb(236, 165, 57)).build())
-        items.add(ChartItemBuilder.create().name("Item 4").value(0).fill(Color.rgb(135, 170, 102)).build())
-        items.add(ChartItemBuilder.create().name("Item 5").value(0).fill(Color.rgb(136, 171, 173)).build())
-        items.add(ChartItemBuilder.create().name("Item 6").value(0).fill(Color.rgb(76, 179, 210)).build())
-        items.add(ChartItemBuilder.create().name("Item 7").value(0).fill(Color.rgb(106, 198, 255)).build())
 
-        chart = BarChartBuilder.create()
-                               .prefSize(600, 300)
-                               .items(items)
-                               .orientation(Orientation.HORIZONTAL)
-                               .backgroundFill(Color.rgb(48, 48, 48))
-                               .namesBackgroundFill(Color.rgb(48, 48, 48))
-                               .barBackgroundFill(Color.rgb(62, 62, 62))
-                               .barBackgroundVisible(true)
-                               .seriesFill(new LinearGradient(0, 0, 1, 0, true, CycleMethod.NO_CYCLE, new Stop(0, Color.rgb(91, 105, 255)), new Stop(1, Color.rgb(76, 41, 217))))
-                               .shadowsVisible(false)
-                               .textFill(Color.WHITE)
-                               .namesTextFill(Color.rgb(190, 190, 190))
-                               .useNamesTextFill(true)
-                               .useItemTextFill(false)
-                               .useItemFill(false)
-                               .shortenNumbers(false)
-                               .sorted(true)
-                               .order(Order.DESCENDING)
-                               //.order(Order.ASCENDING)
-                               //.numberFormat(NumberFormat.PERCENTAGE)
-                               .numberFormat(NumberFormat.NUMBER)
-                               //.animated(false)
-                               .minNumberOfBars(10)
-                               .useMinNumberOfBars(false)
-                               .useGivenColors(true)
-                               .colors(List.of(Color.RED, Color.BLUE, Color.GREEN))
-                               .barCornerRadius(15)
-                               .boldValueFont(true)
-                               .build()
+        // Prepare data for BoxPlot
+        prepareData()
 
-        AnchorPane.setTopAnchor(chart, 10d)
-        AnchorPane.setRightAnchor(chart, 10d)
-        AnchorPane.setBottomAnchor(chart, 10d)
-        AnchorPane.setLeftAnchor(chart, 10d)
+        boxPlot = BoxPlotBuilder.create()
+                                .items(items)
+                                .whiskerStrokeColor(Color.WHITE)
+                                .iqrStrokeColor(Color.WHITE)
+                                .outlierFillColor(Color.WHITE)
+                                .textFillColor(Color.WHITE)
+                                .name("Test")
+                                .decimals(2)
+                                .build()
+                                .asInstanceOf[BoxPlot[ChartItem]]
 
-        lastTimerCall = System.nanoTime()
-        timer = new AnimationTimer() {
-            override def handle(now: Long): Unit = {
-                if (now > lastTimerCall + 3_000_000_000l) {
-                    items.forEach(item => item.setValue(RND.nextDouble() * 75 + 25))
-                    lastTimerCall = now
-                }
-            }
-        }
+        AnchorPane.setTopAnchor(boxPlot, 0d)
+        AnchorPane.setRightAnchor(boxPlot, 0d)
+        AnchorPane.setBottomAnchor(boxPlot, 0d)
+        AnchorPane.setLeftAnchor(boxPlot, 0d)
+
     }
 
     override def start(stage: Stage) = {
-        val pane = new AnchorPane(chart)
+        val pane = new AnchorPane()
+        pane.getChildren().addAll(boxPlot)
+
+
         pane.setBackground(new Background(new BackgroundFill(Color.rgb(48, 48, 48), CornerRadii.EMPTY, Insets.EMPTY)))
+        pane.setPadding(new Insets(10))
 
         val scene = new Scene(pane)
 
-        stage.setTitle("BarChart")
+        stage.setTitle("Box Plot")
         stage.setScene(scene)
         stage.show()
-
-        timer.start()
     }
 
     override def stop() = {
         System.exit(0)
     }
+
+    private def prepareData() = {
+        //List<Double> data = List.of(52.0, 57.0, 57.0, 58.0, 63.0, 66.0, 66.0, 67.0, 67.0, 68.0, 69.0, 70.0, 70.0, 70.0, 70.0, 72.0, 73.0, 75.0, 75.0, 76.0, 76.0, 78.0, 79.0, 89.0)
+        //List<Double> data = List.of(57.0, 57.0, 57.0, 58.0, 63.0, 66.0, 66.0, 67.0, 67.0, 68.0, 69.0, 70.0, 70.0, 70.0, 70.0, 72.0, 73.0, 75.0, 75.0, 76.0, 76.0, 78.0, 79.0, 81.0)
+        val data: List[Double] = List.of(91.00,  95.00,  54.00,  69.00,  80.00,  85.00,  88.00,  73.00,  71.00,  70.00,  66.00,  90.00,  86.00,  84.00,  73.00)
+        data.forEach(v => items.add(new ChartItem(v)))
+    }
+
 
     def launchIt():Unit = {
         Application.launch()
